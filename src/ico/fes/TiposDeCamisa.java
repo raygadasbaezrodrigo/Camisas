0898f07f1db0dfeb9f4bcfe16ffa4ea88faca175
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2020-03-02 17:23:35 -0600

    Update TiposDeCamisa.java

--- src/ico/fes/TiposDeCamisa.java
+++ src/ico/fes/TiposDeCamisa.java
@@ -19,6 +19,7 @@ public class TiposDeCamisa {
         
     }
 
+        //Constructor
     public TiposDeCamisa() {
     }
     
@@ -39,7 +40,7 @@ public class TiposDeCamisa {
         this.precio = precio;
     }
     
-    
+    //Muestra al usuario las opciones a seleccionar
     public void MostrarDatos(){
         System.out.println("Tipo    Descripcion             Precio");
         System.out.println(" 1      Manga Corta              190");
@@ -47,35 +48,38 @@ public class TiposDeCamisa {
         System.out.println(" 3   Formal de manga larga       310");
     }
     
-    
+    //Solicita al usuario la cantidad de cammisas a comprar
     public static int cuantas(){
         System.out.println("Cuantas vas a querer?");
         Scanner opcion = new Scanner(System.in);
         int cantidad;
         cantidad = opcion.nextInt();
-        
         return cantidad;
         
     }
-        
+    
+    //Calcula los descuentos con base en la cantidad solicitada
     public double descuento(int cantidad){
         double descuento; 
         double total=0;
         double descuento1=0.05;
         double descuento2= 0.08;
+        
+        //De 0 a 2 camisas no hay descuento
         if (cantidad<=2){
             total = precio*cantidad;
             System.out.println("No hay descuento,su total es: " + total); 
             return total;
         }
         
-        //Aqui se modifico
+        //De 3 a 5 hay un descuento del 5%
         if (cantidad>2 && cantidad<=5){
             descuento = precio*cantidad*descuento1;
             total= (precio*cantidad)-(descuento);
             System.out.println("Si hay descuento, su total es: " + total);
             return total;
     }
+        //Mayor a 5, hay un descuento del 8%
         if (cantidad>5){
            descuento = precio*cantidad*descuento2;
            total= (precio*cantidad)-(descuento);
