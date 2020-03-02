28a4a00446e789fab1581302a40dc5f12bfb979a
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2020-03-02 17:14:34 -0600

    Update efectivo.java

--- src/ico/fes/efectivo.java
+++ src/ico/fes/efectivo.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class efectivo {
     public static void main(String[] args) {
         
+        //Inicializa los tipos de camisas, con su tipo y su precio 
         TiposDeCamisa camisa1 = new TiposDeCamisa(1, 190);
         TiposDeCamisa camisa2 = new TiposDeCamisa(2, 230);
         TiposDeCamisa camisa3 = new TiposDeCamisa(3, 310);
@@ -23,13 +24,16 @@ public class efectivo {
         System.out.println("Escoge un tipo de camisa: ");
         Scanner opcion = new Scanner(System.in);
         int option;
-        option = opcion.nextInt();
+        option = opcion.nextInt(); //Leer desde el teclado la opcion
+        
+        //Sale del programa si se inserta alguna opcion no valida
    
         if(option !=1 && option !=2 && option!=3){
             System.out.println("Opcion no valida.");
             System.exit(0);
         }
         
+        //Manda a llamar a la funcion "descuento" con base en la opcion seleccionad
         if (option == 1){
             camisa1.descuento(TiposDeCamisa.cuantas());
         }else{
